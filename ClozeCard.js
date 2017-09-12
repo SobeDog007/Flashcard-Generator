47e31e4406dca9209d8e041ca1d7918888015d8a
Steven Sober <sdsober@gmail.com>
2017-09-12 18:30:47 -0400

    Update

--- ClozeCard.js
+++ ClozeCard.js
@@ -5,18 +5,28 @@
 
 // Constructor:
 function ClozeCard(text, cloze) {
-	this.fullText  = text;
-	this.cloze     = cloze;
-	
-	if (this.fullText.includes(cloze))
-	{
-		this.partial = this.fullText.replace(this.cloze, "... ");
-	}
 
+	if (this instanceof ClozeCard)
+	{
+		this.fullText  = text;
+		this.cloze     = cloze;
+		
+		// Validate that cloze is indeed part of the full text.
+		if (this.fullText.includes(cloze))
+		{
+			this.partial = this.fullText.replace(this.cloze, "... ");
+		}
+
+		else
+		{
+			console.log("Error: Cloze not part of text!");
+			this.partial = "";
+		}
+	}
+		
 	else
 	{
-		console.log("Error: Cloze not part of text!");
-		this.partial = "";
+		return new ClozeCard(text, cloze);
 	}
 }
 
@@ -26,13 +36,13 @@ module.exports = ClozeCard;
 // Test Execution
 // ***************************************************************************
 
-// Create a card.
-var firstPresidentCloze = new ClozeCard("George Washington was the first president of the United States.",
-													 "George Washington");
-// Log the card properties.
-console.log("Cloze   : " + firstPresidentCloze.cloze);
-console.log("Partial : " + firstPresidentCloze.partial);
-console.log("FullText: " + firstPresidentCloze.fullText);
+// // Create a card.
+// var firstPresidentCloze = new ClozeCard("George Washington was the first president of the United States.",
+// 													 "George Washington");
+// // Log the card properties.
+// console.log("Cloze   : " + firstPresidentCloze.cloze);
+// console.log("Partial : " + firstPresidentCloze.partial);
+// console.log("FullText: " + firstPresidentCloze.fullText);
 
-// Error Case:
-var brokenCloze = new ClozeCard("This doesn't work", "oops");
+// // Error Case:
+// var brokenCloze = new ClozeCard("This doesn't work", "oops");
